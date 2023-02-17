100889c5431465bffef73731eecdf47425274500
thahira97 <thahirasheerin3@gmail.com>
2023-02-17 16:21:36 -0500

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,9 +13,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, [process.argv[2] || 'JUL02'])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
